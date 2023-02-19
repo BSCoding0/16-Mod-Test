c0a2d938eadd5634e81047ce2303d0d0ba97b18d
BSCoding0 <bscoding0@gmail.com>
2023-02-19 20:03:36 +0100

    Experience Catalyst Fix (Only works with Stacks of 1, so it is now unstackable)

--- src/main/java/com/bsanimation/sigmamod/item/ItemExperienceCatalyst.java
+++ src/main/java/com/bsanimation/sigmamod/item/ItemExperienceCatalyst.java
@@ -6,38 +6,24 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Rarity;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 
 public class ItemExperienceCatalyst extends Item {
     public ItemExperienceCatalyst(Properties settings) {
-        super(settings.group(ItemGroup.MATERIALS).rarity(Rarity.UNCOMMON).isImmuneToFire().maxStackSize(1).maxDamage(1));
+        super(settings.group(ItemGroup.MATERIALS).rarity(Rarity.UNCOMMON).isImmuneToFire().maxStackSize(1));
 
     }
 
-    private boolean right_click = false;
-
-    //gets called when dropped unfortunately
-    @Override
-    public boolean onDroppedByPlayer(ItemStack item, PlayerEntity player) {
-        if(this.right_click) { //if the player right-clicks the set the boolean to true and so the item gets dropped (want to remove it instead, this is a workaround)
-            item.setCount((item.getCount()) - 1);
-            player.inventory.deleteStack(item);
-            player.giveExperiencePoints(100);
-            this.right_click = false;
-            return false;
-        }else{ //the player drops the item normally
-            return true;
-        }
-
-    }
-
-    //drops the item (and deletes it -> I want it to remove it without dropping)
+    //doesn't work with itemStacks that are bigger than 1 -> maxStackSize(1)
     @Override
     public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity player, Hand hand) {
-        this.right_click = true;
-        player.drop(true);
-        return super.onItemRightClick(world, player, hand);
+        ItemStack itemStack = new ItemStack(getItem());
+        itemStack.getStack().setCount((itemStack.getStack().getCount())-1);
+        ActionResult actionResult = new ActionResult(ActionResultType.SUCCESS, itemStack);
+        player.giveExperiencePoints(100);
+        return ActionResult.resultSuccess(itemStack);
     }
 
     @Override
